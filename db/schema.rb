749ba564477da43e572dda4537358ce8df128df1
Freda Suyi Ding <freda.ding@wework.com>
2018-01-09 17:52:53 -0500

    create wallet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109223836) do
+ActiveRecord::Schema.define(version: 20180109225230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20180109223836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uuid"], name: "index_users_on_uuid"
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.float "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
