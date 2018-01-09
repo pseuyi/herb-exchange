5a5554d5019073d018600cc079131e411d0e4745
Freda Suyi Ding <freda.ding@wework.com>
2018-01-09 17:58:46 -0500

    create transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109225725) do
+ActiveRecord::Schema.define(version: 20180109225830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20180109225725) do
   create_table "items", force: :cascade do |t|
     t.integer "price"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
