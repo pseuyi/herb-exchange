fe0cfa72109d9bbd191df51b252878d32c5d0482
Freda Suyi Ding <freda.ding@wework.com>
2018-01-09 18:51:11 -0500

    temp

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :collectible, polymorphic: true
+  belongs_to :item_type
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :items as: :collectible
+  has_many :transactions
 end
