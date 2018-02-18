3d93f5d2f6f09e239d80c7220959554b761af7c0
pseuyi <pseuyi@gmail.com>
2018-02-18 15:47:10 -0500

    add item_types controller

---
+++ app/controllers/api/v1/item_types_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ItemTypesController < ApplicationController
+  def index
+    @item_types = ItemType.order('created_at DESC')
+  end
+
+  def create
+    @item_type = ItemType.create(item_type_params)
+    if @item_type.save
+     response = { message: "Item type #{item_type_params[:name]} created!", item_type: @item_type }
+     render json: response, status: :created
+    else
+     render json: @item_type.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_type_params
+    params.require(:item_type).permit(:name, :description, :category)
+  end
+end
