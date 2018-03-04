36faa089591b1bbc241859427c69b2d0ee757acc
pseuyi <pseuyi@gmail.com>
2018-03-03 20:22:01 -0500

    load s3 credential from yaml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :bucket => 'herb-garden-attachments',
-    :s3_region => 'us-east-1'
+    :s3_region => 'us-east-1',
+    :s3_credentials => YAML.load_file(Rails.root.join("config/aws.yml"))
   }
 end
