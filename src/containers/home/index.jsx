ce8ce6877fe4a673bd21c4f5e16dec9be611f237
pseuyi <pseuyi@gmail.com>
2018-03-17 15:15:26 -0400

    create navigation component

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from 'components/header';
-import { Link } from 'react-router-dom';
+import Navigation from 'containers/navigation';
 
 class Home extends React.Component {
-
   render() {
     return (
       <div>
         <Header/>
+        <Navigation/>
         <img src="/assets/tree.png"/>
-        <ul>
-          <li><Link to="/field">to field</Link></li>
-        </ul>
       </div>
     )
   }
---
+++ src/containers/navigation/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withRouter, NavLink, Route } from 'react-router-dom';
+
+class Navigation extends React.Component {
+  render () {
+    return (
+      <ul className="navigation">
+        <li><NavLink exact to="/">to home</NavLink></li>
+        <li><NavLink to="/field">to field</NavLink></li>
+      </ul>
+    )
+  }
+}
+
+export default Navigation;
+// <li><Link to="/field">back</Link></li>
