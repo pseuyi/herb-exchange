8d1a875a948c26559c3609f3833bbbddd8e3c83a
pseuyi <pseuyi@gmail.com>
2018-04-07 16:40:05 -0400

    add markets page and new market form

--- src/containers/navigation/index.jsx
+++ src/containers/navigation/index.jsx
@@ -7,6 +7,7 @@ class Navigation extends React.Component {
       <ul className="navigation">
         <li><NavLink exact to="/">to home</NavLink></li>
         <li><NavLink to="/field">to field</NavLink></li>
+        <li><NavLink to="/markets">to markets</NavLink></li>
       </ul>
     )
   }
