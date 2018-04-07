dd2a8eb54a557e38a834a54cff2fcb020afde4ec
pseuyi <pseuyi@gmail.com>
2018-04-07 16:39:32 -0400

    add markets reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import types from './ducks/types';
+import markets from './ducks/markets';
 
 export default combineReducers({
   types,
+  markets,
   routing: routerReducer
 });
